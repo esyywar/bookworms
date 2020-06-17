8591e574ea125e858321de2b6dd83f019806b50d
esyywar <rahuleswar@gmail.com>
2020-06-16 22:57:35 -0400

    user form schema and validation

--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
-const mongoose = require("mongoose")
-const config = require("config")
-const db = config.get("mongoUri")
+const mongoose = require('mongoose')
+const config = require('config')
+const db = config.get('mongoUri')
 
+/* Connecting to mongoose database */
 async function connectDB() {
-  try {
-    await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-    console.log("MongoDB connected")
-  } catch (err) {
-    console.log(err.message)
-    process.exit(1)
-  }
+	try {
+		await mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+		console.log('MongoDB connected')
+	} catch (err) {
+		console.log(err.message)
+		process.exit(1)
+	}
 }
 
 module.exports = connectDB
