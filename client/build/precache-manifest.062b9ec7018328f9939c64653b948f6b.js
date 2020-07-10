e7074b78ae0daa3a8432b0f7533705adf030b56a
esyywar <rahuleswar@gmail.com>
2020-07-10 00:59:31 -0400

    ready for deploy to heroku

---
+++ client/build/precache-manifest.062b9ec7018328f9939c64653b948f6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac883befe929d6f509758d19ade1533f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a705d5322169bf63a86",
+    "url": "/static/css/main.97305261.chunk.css"
+  },
+  {
+    "revision": "2cd66969a6b2d7ef9523",
+    "url": "/static/js/2.9178b889.chunk.js"
+  },
+  {
+    "revision": "005c97f82cee60e20eac659314ea29ec",
+    "url": "/static/js/2.9178b889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a705d5322169bf63a86",
+    "url": "/static/js/main.ec0a65e2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const connectDB = require('./config/db')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const app = express()
 
@@ -11,16 +12,22 @@ connectDB()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 
-/* Set port as production server or 5000 for development */
-const PORT = process.env.PORT || 5000
-
-/* Get at root index */
-app.get('/', (req, res) => res.send('API running'))
-
 /* Setting up API routes */
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/users', require('./routes/api/users'))
+
+/* If in production, set static folder to client index.html */
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('/client/build'))
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
+}
+
+/* Set port as production server or 5000 for development */
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
